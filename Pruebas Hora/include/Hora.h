1e9c418fd0f17d3d7a45164d44c43f64ff79e98a
BvM94 <mauu.bernabeu1994@gmail.com>
2019-06-21 15:55:04 -0300

    Clase hora con las pruebas

---
+++ Pruebas Hora/include/Hora.h
@@ -0,0 +1,36 @@
+#ifndef HORA_H
+#define HORA_H
+
+#include <iostream>
+
+using namespace std;
+
+class Hora
+{
+    private:
+        int seg;
+
+    public:
+        Hora(int h , int m , int s); //constructor
+        Hora();                         //constructor por defecto
+        static void validar_hora(int h, int m, int s);
+
+
+        Hora operator+(const Hora& otra)const;
+        Hora operator-(const Hora& otra)const;
+        bool operator>(const Hora& otra)const;
+        bool operator<(const Hora& otra)const;
+        bool operator<=(const Hora& otra)const;
+        bool operator>=(const Hora& otra)const;
+        bool operator==(const Hora& otra)const;
+        bool operator!=(const Hora& otra)const;
+
+        void get_HMS(int &h , int &m, int &s);
+
+
+    protected:
+};
+
+iostream& operator<<(iostream& sal ,Hora & hora);
+
+#endif // HORA_H
---
+++ Pruebas Hora/src/Hora.cpp
@@ -0,0 +1,82 @@
+#include "Hora.h"
+
+Hora::Hora(int h , int m , int s) //constructor
+{
+    validar_hora(h,m,s);
+    this->seg = h * 3600 + m *60 + s;
+}
+
+Hora::Hora()
+{
+    this->seg = 0;
+}
+
+void Hora::validar_hora(int h, int m, int s)
+{
+    if( h<0 || m<0 || m>=60 || s>=60 || s<0 )
+       throw exception("Fecha Invalida");
+}
+
+Hora Hora::operator+(const Hora& otra)const
+{
+    Hora rs;
+
+    rs.seg = this->seg + otra.seg;
+
+    return rs;
+}
+
+Hora Hora::operator-(const Hora& otra)const
+{
+    Hora rs;
+
+    rs.seg = this->seg - otra.seg;
+
+    return rs;
+}
+
+bool Hora::operator>(const Hora& otra)const
+{
+    return this->seg > otra.seg;
+}
+
+bool Hora::operator<(const Hora& otra)const
+{
+    return this->seg < otra.seg;
+}
+
+bool Hora::operator==(const Hora& otra)const
+{
+    return this->seg == otra.seg;
+}
+
+bool Hora::operator>=(const Hora& otra)const
+{
+    return this->seg >= otra.seg;
+}
+
+bool Hora::operator<=(const Hora& otra)const
+{
+    return this->seg <= otra.seg;
+}
+
+bool Hora::operator!=(const Hora& otra)const
+{
+    return this->seg != otra.seg;
+}
+
+void Hora::get_HMS(int &h , int &m, int &s)
+{
+    h = this->seg / 3600;
+    m = this->seg % 3600 /60;
+    s = this->seg % 60;
+}
+
+iostream& operator<<(iostream& sal ,Hora & hora)
+{
+    int h,m,s;
+    hora.get_HMS(h,m,s);
+
+    sal<<h<<":"<<m<<":"<<s;
+    return sal;
+}
